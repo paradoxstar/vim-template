4ce5837d108f59b3c985d9d96d00e11679f4a6e4
Owen Yu <staryhk@gmail.com>
2016-10-24 16:19:13 +0800

    modified py

--- templates/=template=.py
+++ templates/=template=.py
@@ -4,8 +4,9 @@
 #
 # Copyright © %YEAR% %USER% <%MAIL%>
 #
-# Distributed under terms of the %LICENSE% license.
+# @AUTHOR:      %USER%
+# @MAIL:        %MAIL%
+# @VERSION:     %DATE%
+#
 
-"""
 %HERE%
-"""
